bfddcc9e67d78bf3cf33a63d66bed29e8e439cb6
dlmedv <114763924+dlmedv@users.noreply.github.com>
2023-06-17 16:19:35 +0300

    изменяет адрес подключения в базе данных

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { validateSignIn, validateSignUp } = require('./middlewares/validations');
 
 const app = express();
 const { PORT = 3000 } = process.env;
-mongoose.connect('mongodb://localhost:27017/mestodb', {});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 
 app.use(helmet());
 app.use(express.json());
