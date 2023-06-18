6d0093d141062ce895029396614d80d74d3ee9bc
dlmedv <114763924+dlmedv@users.noreply.github.com>
2023-06-18 17:54:56 +0300

    добавляет фронт и бек

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co"
+export const BASE_URL = "https://api.dlmedvmesto.nomoredomains.rocks"
 
 export const checkStatusRes = (res) => {
     if (res.ok) {
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,8 +1,8 @@
 
 export const options = {
-    url: 'https://mesto.nomoreparties.co/v1/cohort-60',
+    url: "https://api.dlmedvmesto.nomoredomains.rocks",
     headers: {
-        authorization: 'e24dd49a-4101-455b-9fb5-311038dfd707',
+        //authorization: 'e24dd49a-4101-455b-9fb5-311038dfd707',
         'Content-Type': 'application/json'
     }
 }
