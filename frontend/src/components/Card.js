69754b0168f8e662b5329ec7bf4b5c471d77d9c7
dlmedv <114763924+dlmedv@users.noreply.github.com>
2023-06-20 15:12:55 +0300

    реалищация запросов лайка

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,7 +4,7 @@ import { CurrentUserContext } from '../contexts/CurrentUserContext'
 function Card(props) {
     const currentUser = useContext(CurrentUserContext);
     const isOwn = props.card.owner === currentUser._id;
-    const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+    const isLiked = props.card.likes.some(i => i === currentUser._id);
     const cardLikeButtonClassName = (`element__icon + ${isLiked && 'element__icon_active'}`)
 
     function handleClick() {
